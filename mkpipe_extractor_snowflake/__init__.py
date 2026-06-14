9e2dd3745e2dbe5c700fbbf8a866ebfc236e58a9
m-karakus <s.metinkarakus@gmail.com>
2026-06-15 00:31:55 +0300

    hotfix: filter_lower_bound and filter_upper_bound

--- mkpipe_extractor_snowflake/__init__.py
+++ mkpipe_extractor_snowflake/__init__.py
@@ -121,7 +121,7 @@ class SnowflakeExtractor(BaseExtractor, variant='snowflake'):
                     else:
                         conditions.append(f"{table.iterate_column} < '{table.filter_upper_bound}'")
                 filter_clause = 'WHERE ' + ' AND '.join(conditions)
-                write_mode = 'overwrite'
+                write_mode = 'append'
             elif last_point:
                 write_mode = 'append'
                 if table.iterate_column_type == 'int':
